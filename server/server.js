27c2e3c38e2e99c2350bbba77c7a27d62647ded4
Facundo Cristaldo <facu.cristaldo.96@gmail.com>
2021-03-20 20:06:03 -0300

    starting with source given

--- server/server.js
+++ server/server.js
@@ -1,18 +1,28 @@
-const { gql, ApolloServer } = require("apollo-server");
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const express = require('express');
+const expressJwt = require('express-jwt');
+const jwt = require('jsonwebtoken');
+const db = require('./db');
 
-const typeDefs = gql`
-  type Query{
-    greeting: String
+const port = 9000;
+const jwtSecret = Buffer.from('Zn8Q5tyZ/G1MHltc4F/gTkVJMlrbKiZt', 'base64');
+
+const app = express();
+app.use(cors(), bodyParser.json(), expressJwt({
+  secret: jwtSecret,
+  credentialsRequired: false
+}));
+
+app.post('/login', (req, res) => {
+  const {email, password} = req.body;
+  const user = db.users.list().find((user) => user.email === email);
+  if (!(user && user.password === password)) {
+    res.sendStatus(401);
+    return;
   }
-`
+  const token = jwt.sign({sub: user.id}, jwtSecret);
+  res.send({token});
+});
 
-//Resolvers need to match typeDefs 
-const resolvers = {
-  Query: {
-    greeting: () => "Hello GraphQL"
-  }
-}
-
-const server = new ApolloServer({ typeDefs, resolvers })
-
-server.listen({ port: 9000 }).then(serverInfo => console.log(`Server listening at ${serverInfo.url}`))
+app.listen(port, () => console.info(`Server started on port ${port}`));
