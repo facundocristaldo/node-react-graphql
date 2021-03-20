9e61c207fee4b13f831b8601364e2a983f066f55
Facundo Cristaldo <facu.cristaldo.96@gmail.com>
2021-03-20 19:55:28 -0300

    starting the server and resolving first query

--- server/server.js
+++ server/server.js
@@ -1,7 +1,18 @@
-const { gql } = require("apollo-server");
+const { gql, ApolloServer } = require("apollo-server");
 
 const typeDefs = gql`
   type Query{
     greeting: String
   }
 `
+
+//Resolvers need to match typeDefs 
+const resolvers = {
+  Query: {
+    greeting: () => "Hello GraphQL"
+  }
+}
+
+const server = new ApolloServer({ typeDefs, resolvers })
+
+server.listen({ port: 9000 }).then(serverInfo => console.log(`Server listening at ${serverInfo.url}`))
